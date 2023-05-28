b21dab292ec7df07c08e59b0ec52f84411ab75e8
ivangayton <ivangayton@gmail.com>
2023-05-28 22:43:21 +0900

    all the way to concave hulls

--- group_thoplo_task_splitting_algorithm/task_splitting.sql
+++ group_thoplo_task_splitting_algorithm/task_splitting.sql
@@ -1,7 +1,6 @@
--- The Area of Interest boundary provided by the person creating the project
-WITH boundary AS (
-  SELECT ST_Boundary(geom) AS geom
-  FROM "project_aoi"
+-- The Area of Interest provided by the person creating the project
+WITH aoi AS (
+  SELECT * FROM "project_aoi"
 )
 -- Extract all lines to be used as splitlines from a table of lines
 -- with the schema from Underpass (all tags as jsonb column called 'tags')
@@ -12,16 +11,10 @@ WITH boundary AS (
 -- so that they're easily configured during project creation.
 ,splitlines AS (
   SELECT ST_Intersection(a.geom, l.geom) AS geom
-  FROM "project_aoi" a, "ways_line" l
+  FROM aoi a, "ways_line" l
   WHERE ST_Intersects(a.geom, l.geom)
-  AND (tags->>'highway' = 'trunk'
-    OR tags->>'highway' = 'primary'
-    OR tags->>'highway' = 'secondary'
-    OR tags->>'highway' = 'tertiary'
-    OR tags->>'highway' = 'residential'
-    OR tags->>'highway' = 'unclassified'
-    OR tags->>'waterway' = 'river'
-    OR tags->>'waterway' = 'drain'
+  AND (tags->>'highway' IS NOT NULL
+    OR tags->>'waterway' IS NOT NULL
     OR tags->>'railway' IS NOT NULL
     )
 )
@@ -31,12 +24,17 @@ WITH boundary AS (
   FROM splitlines
 )
 -- Combine the boundary of the AOI with the splitlines
--- TODO: Is it better to do this before merging?
+-- Extract the Area of Interest boundary as a line
+,boundary AS (
+  SELECT ST_Boundary(geom) AS geom
+  FROM aoi
+)
+-- And combine it with the splitlines
 ,comb AS (
   SELECT ST_Union(boundary.geom, merged.geom) AS geom
   FROM boundary, merged
 )
--- Polygonize; create a polygon for each area enclosed by the splitlines
+-- Create a polygon for each area enclosed by the splitlines
 ,splitpolysnoindex AS (
   SELECT (ST_Dump(ST_Polygonize(comb.geom))).geom as geom
   FROM comb
@@ -47,6 +45,7 @@ WITH boundary AS (
 SELECT row_number () over () as polyid, * 
 from splitpolysnoindex
 )
+--
 -- Grab the buildings.
 -- While we're at it, grab the ID of the polygon the buildings fall within.
 -- TODO: at the moment this uses ST_Intersects, which is fine except when
@@ -77,9 +76,14 @@ from splitpolysnoindex
   FROM polygonsfeaturecount pfc
   WHERE pfc.numfeatures > 0
 )
+/* ***********************************
+-- Uncomment this and stop here for split polygons before clustering
+SELECT * FROM taskpolygons
+*************************************/
+
 -- Add the count of features in the splitpolygon each building belongs to
 -- to the buildings table; sets us up to be able to run the clustering
-,buildingstocluster AS (
+,buildingswithcount AS (
   SELECT b.*, p.numfeatures
   FROM buildings b 
   LEFT JOIN polygonsfeaturecount p
@@ -90,15 +94,43 @@ from splitpolysnoindex
 -- so we're dividing the number of features by a constant (10 in this case)
 -- to get the number of clusters required to get close to the right number
 -- of features per cluster.
--- TODO: this should certainly not be a hardcoded, the number of features
--- per cluster should come from a project configuration table
+-- TODO: This should certainly not be a hardcoded, the number of features
+--       per cluster should come from a project configuration table
+-- TODO; CRITICAL: Sets of buildings in polygons with less than twice the
+--                 number of desired buildings plus one
+--                 per task must be excluded from this operation
+,buildingstocluster as (
+  SELECT * FROM buildingswithcount bc
+  WHERE bc.numfeatures > 21
+)
 ,clusteredbuildings AS (
-SELECT 
-  *,
+SELECT *,
   ST_ClusterKMeans(geom, cast((b.numfeatures / 10) + 1 as integer)) 
   over (partition by polyid) as cid
 FROM buildingstocluster b
 )
-
-select * from clusteredbuildings
-
+/* ***********************************
+-- Uncomment this and stop here for clustered buildings
+SELECT * FROM cluteredbuildings
+*************************************/
+,hulls AS(
+  -- Using a very high param_pctconvex value; smaller values often produce
+  -- self-intersections and crash. It seems that anything below 1 produces
+  -- something massively better than 1 (which is just a convex hull) but
+  -- no different (i.e. 0.99 produces the same thing as 0.9999), so
+  -- there's nothing to lose choosing a value a miniscule fraction less than 1.
+  select ST_ConcaveHull(ST_Collect(clb.geom), 0.9999) as geom
+  from clusteredbuildings clb
+  group by clb.cid, clb.polyid
+)
+-- Now we need to:
+--   - Create intersections for the hulls so all overlapping bits are separated
+--   - Check what's inside of the overlapping bits
+--     - If it's only stuff belonging to one of the original hulls, give that
+--       bit to the hull it belongs to
+--     - If the overlapping are contains stuff belonging to more than one hull,
+--       somehow split the overlapping bit such that each piece only contains
+--       stuff from one or another parent hull. Then merge them back.
+--  - Do something Voronoi-esque to expand the hulls until they tile the
+--    entire AOI, creating task polygons with no gaps
+select * from hulls
