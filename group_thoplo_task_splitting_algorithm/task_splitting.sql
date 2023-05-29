c29e57fa9d08a1703a24be69e1da1dd8af93b0f6
Stephen Mather <1174901+smathermather@users.noreply.github.com>
2023-05-28 23:05:28 -0400

    insert magic function

--- group_thoplo_task_splitting_algorithm/task_splitting.sql
+++ group_thoplo_task_splitting_algorithm/task_splitting.sql
@@ -132,26 +132,14 @@ FROM buildingstocluster b
 )
 /* ***********************************
 -- Uncomment this and stop here for clustered buildings
-SELECT * FROM cluteredbuildings
+SELECT * FROM clusteredbuildings
 *************************************/
+-- magic function that takes in building geometry and bounding geometry
+-- and returns 
 ,hulls AS(
-  -- Using a very high param_pctconvex value; smaller values often produce
-  -- self-intersections and crash. It seems that anything below 1 produces
-  -- something massively better than 1 (which is just a convex hull) but
-  -- no different (i.e. 0.99 produces the same thing as 0.9999), so
-  -- there's nothing to lose choosing a value a miniscule fraction less than 1.
-  select ST_ConcaveHull(ST_Collect(clb.geom), 0.9999) as geom
+  select Ivan_Hull(ST_Collect(clb.geom), clb.clusterid, tsk.geom) as geom
   from clusteredbuildings clb
-  group by clb.cid, clb.polyid
+  LEFT JOIN taskpolygons ON clb.polyid = tsk.polyid
 )
--- Now we need to:
---   - Create intersections for the hulls so all overlapping bits are separated
---   - Check what's inside of the overlapping bits
---     - If it's only stuff belonging to one of the original hulls, give that
---       bit to the hull it belongs to
---     - If the overlapping are contains stuff belonging to more than one hull,
---       somehow split the overlapping bit such that each piece only contains
---       stuff from one or another parent hull. Then merge them back.
---  - Do something Voronoi-esque to expand the hulls until they tile the
---    entire AOI, creating task polygons with no gaps
+
 select * from hulls
